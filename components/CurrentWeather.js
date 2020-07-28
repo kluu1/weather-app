8dff8abffee5d900dcbcded022cc153056a5f41e
Kevin Luu <kluu@epictech.cm>
2020-07-28 07:46:31 -0400

    render image from desc

--- components/CurrentWeather.js
+++ components/CurrentWeather.js
@@ -3,6 +3,14 @@ import { ToastContainer } from 'react-toastify';
 import { convertTemp } from '../utils';
 
 const CurrentWeather = ({ data, city }) => {
+  const renderImage = () => {
+    if (data.weather[0].description.includes('clear')) {
+      return <Image src="sunny.png" />;
+    } else if (data.weather[0].description.includes('cloud')) {
+      return <Image src="sunny_with_clouds.png" />;
+    }
+  };
+
   return (
     <div className="container">
       <ToastContainer />
@@ -13,7 +21,7 @@ const CurrentWeather = ({ data, city }) => {
               <Card.Description>
                 <h2>Current Weather in {city}</h2>
                 <hr />
-                <Image src="sunny.png" />
+                {renderImage()}
                 <h3>Feels like: {convertTemp(data.main.feels_like)} F</h3>
                 <h4>{data.weather[0].description}</h4>
                 <div>Temp: {convertTemp(data.main.temp)} F</div>
